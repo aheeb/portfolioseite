4a9a55594fe40abe64140a4d37397328e3600dbd
aheeb <78415840+aheeb@users.noreply.github.com>
2023-10-02 09:52:58 +0200

    done

--- lebenslauf.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', function() {
-    let scrollY = window.scrollY;
-    let docHeight = document.documentElement.scrollHeight - window.innerHeight;
-    let pathLength = document.getElementById('snake').getTotalLength();
-
-    let offset = pathLength * ((docHeight - scrollY) / docHeight);
-
-    document.getElementById('snake').style.strokeDashoffset = offset;
-});
--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-let button = document.querySelector("button");
-let clicked = false;
-button.addEventListener("click", function() {
-    let menue = document.querySelector(".mobile");
-    if (clicked) {
-        menue.style.maxHeight = "0";  /* Menü schließen */
-        clicked = false;
-    } else {
-        menue.style.maxHeight = menue.scrollHeight + "px";  /* Menü öffnen. scrollHeight gibt die Höhe des Inhalts im Menü zurück. */
-        clicked = true;
+document.addEventListener("DOMContentLoaded", function() {
+    const canvas = document.getElementById('snakeCanvas');
+    const ctx = canvas.getContext('2d');
+    const points = [
+      document.getElementById('point1'),
+      document.getElementById('point2'),
+      // Add more points here
+    ];
+    let currentPointIndex = 0;
+  
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  
+    let snake = [{x: 10, y: 10}];
+  
+    function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+      let target = points[currentPointIndex].getBoundingClientRect();
+      let head = snake[0];
+  
+      let dx = target.left - head.x;
+      let dy = target.top - head.y;
+  
+      let distance = Math.sqrt(dx * dx + dy * dy);
+  
+      if (distance < 1) {
+        currentPointIndex = (currentPointIndex + 1) % points.length;
+      } else {
+        dx /= distance;
+        dy /= distance;
+        head.x += dx;
+        head.y += dy;
+      }
+  
+      ctx.fillStyle = 'green';
+      ctx.fillRect(head.x, head.y, 20, 20);
+  
+      requestAnimationFrame(draw);
     }
-});
+  
+    draw();
+  });
+  
